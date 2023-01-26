14247ae7efb93f006a1bb3b8f8be896aa40d7a79
Ángel Heredia <33127189+anthares101@users.noreply.github.com>
2023-01-26 11:09:14 +0100

    Add comments to config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-CHAT_ID = 123
-TOKEN = 'TOKEN'
-HOSTNAME = 'HOSTNAME'
+CHAT_ID = 123 # The ID of the chat / group you have with the bot
+TOKEN = 'TOKEN' # Your bot token
+HOSTNAME = 'HOSTNAME' # The name of the host the bot is running on
